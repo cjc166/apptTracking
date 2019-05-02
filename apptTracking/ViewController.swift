b1c76fa7bd3c63ebbc97289084065ae1f791c5f3
Caleb Cain <cjc166@case.edu>
2019-05-02 12:49:41 -0400

    Initial Commit

---
+++ apptTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apptTracking
+//
+//  Created by Caleb Cain on 5/2/19.
+//  Copyright © 2019 Caleb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
